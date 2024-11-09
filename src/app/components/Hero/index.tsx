5e1639ba52817f8583d062171ea53d3c1cf20a60
davidebaldi <d.baldi88@gmail.com>
2024-11-09 21:23:51 +0100

    fixing site under construction message

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -30,7 +30,7 @@ const Banner: React.FC<BannerProps> = ({ page }) => {
     case "home":
       src = "/images/hero31.jpg";
       alt = "Home Banner";
-      imageClass = "h-screen object-cover md:h-[calc(100vh-80px)]";
+      imageClass = "h-screen object-cover md:h-[calc(150vh-80px)]";
       bannerContent = (
         <div
           className=" absolute inset-0 flex flex-col items-center sm:items-start justify-center text-white text-left px-4 md:px-8 lg:px-20 xl:px-32"
@@ -39,34 +39,18 @@ const Banner: React.FC<BannerProps> = ({ page }) => {
           <div className="OVERLAY bg-black opacity-35 absolute w-full h-full z-20 top-0 left-0"></div>
           <div className="z-30 flex flex-col w-[90%]">
             <h1 className="order-3 sm:order-1 mb-8 xsm:mb-16 sm:mb-4 z-30 -ml-4 sm:ml-0 button-text text-md xsm:text-lg sm:text-xl md:text-2xl ">
-            PLACEHOLDER
+            
             </h1>
             <p className="order-1 sm:order-2 z-30 -ml-4 sm:ml-0 text-[31px] xsm:text-[34px] sm:text-5xl md:text-6xl md:mb-2 ">
              
             </p>
-            <p className="order-2 sm:order-3 z-30 -ml-4 sm:ml-0 text-[31px] xsm:text-[34px] sm:text-5xl md:text-6xl mb-4 xsm:mb-10 ">
-            PLACEHOLDER
+            <p className="order-2 sm:order-3 z-30 -ml-4 sm:ml-0 text-[31px] xsm:text-[34px] sm:text-7xl md:text-6xl mb-4 xsm:mb-10 ">
+            WEBSITE UNDER CONSTRUCTION
             </p>
           </div>
-          <div className="z-30 flex flex-col md:flex-row md:space-x-4 md:space-y-0 space-y-4">
-            <Button backgroundColor={"#42B5DD"} hoverBackgroundColor={"#50C878"} textColor={""} {...ContactButton} />
-            <Button backgroundColor={"#7CB9E8"} hoverBackgroundColor={"#F0F8FF"} textColor={"black"} {...ContactButtonBlue} />{/*aero /hove alice blue */}
-            <Button backgroundColor={"#B9D9EB"} hoverBackgroundColor={"#4B9CD3"} textColor={"black"} {...ContactButtonBlue} />{/*colmbia blue /carolina blue */}
-            <Button backgroundColor={"#ADD8E6"} hoverBackgroundColor={"#E1EBEE"} textColor={"black"} {...ContactButtonBlue} />{/*light blue /carolina blue */}
-          </div>
+         
           <div className="z-30 absolute inset-x-0 bottom-0 h-32 md:h-16 flex flex-wrap md:flex-nowrap justify-around items-center text-pt-navyblue p-4 bg-pt-gray2 ">
-            <div className="w-[139px] md:w-auto  md:text-left mb-2 md:mb-0">
-              <p>PLACEHOLDER</p>
-            </div>
-            <div className="w-[139px] md:w-auto  md:text-right mb-2 md:mb-0">
-              <p>PLACEHOLDER</p>
-            </div>
-            <div className="w-[139px] md:w-auto  md:text-left">
-              <p>PLACEHOLDER</p>
-            </div>
-            <div className="w-[139px] md:w-auto  md:text-right">
-              <p>PLACEHOLDER</p>
-            </div>
+            
           </div>
         </div>
       );
